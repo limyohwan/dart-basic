b34f7cab3430d54bf093fa900b0cb4f9767d7f93
Yohwan Lim <dyghks7102@naver.com>
2024-05-13 23:35:11 +0900

    feat: map, where, reduce, fold, cascade operator, chaining 정리

--- bin/functional_programming.dart
+++ bin/functional_programming.dart
@@ -34,5 +34,129 @@ void main() {
 
   print(parsed);
 
+  Map<String, String> harryPorter = {
+    'Harry Potter' : '해리포터',
+    'Ron Weasley' : '론 위즐리',
+    'Hermione Granger' : '헤르미온느 그레인저'
+  };
 
+  final result = harryPorter.map((key, value) => MapEntry('Harry Potter Character $key', '해리포터 캐릭터 $value'));
+  
+  print(harryPorter);
+  print(result);
+
+  final keys = harryPorter.keys.map((e) => 'Harry Potter Character $e').toList();
+  final values = harryPorter.values.map((e) => '해리포터 캐릭터 $e').toList();
+  
+  print(keys);
+  print(values);
+
+  Set blackPinkSet2 = {
+    'rose',
+    'jisoo',
+    'jenny',
+    'lisa'
+  };
+
+  final newSet = blackPinkSet2.map((e) => 'blackpink $e').toSet();
+
+  print(newSet);
+
+  List<Map<String, String>> people = [
+    {
+      'name' : 'rose',
+      'group' : 'blackpink'
+    },
+    {
+      'name' : 'jisoo',
+      'group' : 'blackpink'
+    },
+    {
+      'name' : 'rm',
+      'group' : 'bts'
+    },
+    {
+      'name' : 'v',
+      'group' : 'bts'
+    }
+  ];
+
+  print(people);
+
+  final blackpink = people.where((element) => element['group'] == 'blackpink');
+
+  print(blackpink);
+
+  List<int> numbers = [
+    1,
+    3,
+    5,
+    7,
+    9
+  ];
+
+  final result2 = numbers.reduce((total, value){
+    print('value : $total   element : $value');
+    return total + value;
+  });
+
+  print(result2);
+
+  List<String> words = [
+    '안녕하세요',
+    '저는',
+    '임요환입니다.'
+  ];
+
+  final sentence = words.reduce((value, element) => '$value $element');
+  
+  print(sentence);
+
+  // words.reduce((value, element) => value.length + element.length); // 같은 타입이여야만 함 -> fold를 사용하여 해결
+
+  final sum = numbers.fold<int>(0, (previousValue, element) => previousValue + element);
+  print(sum);
+
+  final sentence2 = words.fold<String>('', (previousValue, element) => previousValue + element);
+  print(sentence2);
+
+  final count = words.fold<int>(0, (previousValue, element) => previousValue + element.length);
+  print(count);
+
+  List<int> even = [2,4,6,8];
+  List<int> odd = [1,3,5,7];
+
+  print([even, odd]); // 리스트 안에 리스트
+  print([...even, ...odd]); // 하나의 리스트로 합쳐짐, cascade 연산자
+
+  print(people);
+  
+  final parsedPeople = people.map((e) => Person(name : e['name']!, group : e['group']!)).toList();
+  print(parsedPeople);
+
+  for(Person person in parsedPeople) {
+    print(person.name);
+    print(person.group);
+  }
+
+  final bts = parsedPeople.where((element) => element.group == 'bts');
+  print(bts);
+
+  final result3 = people
+    .map((e) => Person(name : e['name']!, group : e['group']!))
+    .where((element) => element.group == 'bts')
+    .fold<int>(0, (previousValue, element) => previousValue + element.name.length);
+  print(result3);
+}
+
+class Person {
+  final String name;
+  final String group;
+
+  Person({required this.name, required this.group});
+
+  @override
+  String toString() {
+    return 'Person(name:$name, group:$group)';
+  }
 }
